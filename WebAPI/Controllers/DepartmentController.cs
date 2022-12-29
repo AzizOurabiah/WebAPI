eaed27b00ba7aa713b87ef9e8143ef5086d2c0eb
Aziz-Ourabiah-Hotmail <ourabiahaziz1@gmail.com>
2022-12-29 09:42:14 -0500

    Création du controler Department

---
+++ WebAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
